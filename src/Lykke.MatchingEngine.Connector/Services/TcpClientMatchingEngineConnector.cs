9492a5aa72701e1a4db26281183065eb071e3309
Nikoloz Gamkrelidze <nika@Nikolozs-MacBook-Pro.local>
2017-11-27 17:09:40 +0400

    overdraft added

--- src/Lykke.MatchingEngine.Connector/Services/TcpClientMatchingEngineConnector.cs
+++ src/Lykke.MatchingEngine.Connector/Services/TcpClientMatchingEngineConnector.cs
@@ -157,9 +157,9 @@ namespace Lykke.MatchingEngine.Connector.Services
         }
 
         public async Task<MeResponseModel> TransferAsync(string id, string fromClientId,
-            string toClientId, string assetId, double amount, string feeClientId, double feeSizePercentage)
+            string toClientId, string assetId, double amount, string feeClientId, double feeSizePercentage, double overdraft)
         {
-            var model = MeNewTransferModel.Create(id, fromClientId, toClientId, assetId, amount, feeClientId, feeSizePercentage);
+            var model = MeNewTransferModel.Create(id, fromClientId, toClientId, assetId, amount, feeClientId, feeSizePercentage, overdraft);
             var resultTask = _newTasksManager.Add(id);
 
             if (!await _tcpOrderSocketService.SendDataToSocket(model))
